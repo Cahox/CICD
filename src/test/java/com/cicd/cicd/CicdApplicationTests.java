8bd808d7051caa5b43b28858c12d2ae5786e1e96
Cahox <calvinhong123@gmail.com>
2022-04-11 13:57:52 +0200

    start okteto

---
+++ src/test/java/com/cicd/cicd/CicdApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cicd.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration
+class CicdApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
